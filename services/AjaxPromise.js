2b0ee9d6153748628662d02edca09ee4b7494704
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-03-10 05:10:05 +0530

    Added:Code to view Employee Data from JSON Server using AJAX and Promise

--- services/AjaxPromise.js
+++ services/AjaxPromise.js
@@ -1,22 +1,22 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 function makePromiseCall(methodType, url, async = true, data = null) {
     return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() { 
+      let xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() { 
           //whatever connection changes happens it gives us call back
-            if (xhr.readyState === 4) {
+        if (xhr.readyState === 4) {
               //Matching all 200 Series Responses
-                if (xhr.status === 200 || xhr.status === 201) {
-                    resolve(xhr.responseText);
-                }else if (xhr.status >= 400) {
-                  reject({
-                    status: xhr.status,
-                    statusText: xhr.statusText
-                  });
-                    console.log("Handle 400 Client Error or 500 Server Error ");
-              }
+          if (xhr.status === 200 || xhr.status === 201) {
+              resolve(xhr.responseText);
+          }else if (xhr.status >= 400) {
+              reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+              });
+                console.log("Handle 400 Client Error or 500 Server Error ");
             }
         }
+      }
         xhr.open(methodType, url, async); // open connection
         if(data) {
           console.log(JSON.stringify(data));
@@ -24,7 +24,7 @@ function makePromiseCall(methodType, url, async = true, data = null) {
           xhr.send(JSON.stringify(data));
         }else xhr.send();
           console.log(methodType+" request sent to the server");
-  });
+    });
 }
 
 const getURL = "http://localhost:3000/employees/1";
@@ -34,13 +34,13 @@ makePromiseCall("GET", getURL, true)
         })
         .catch(error => console.log("GET Error Status: " +JSON.stringify(error)));
   
+
 const deleteURL = "http://localhost:3000/employees/4";
 makePromiseCall("DELETE", deleteURL, false)
         .then(responseText =>{
           console.log("User Deleted : " + responseText);
         })
         .catch(error =>console.log("DELETE Error Status: " +JSON.stringify(error)));
-
 
 
 const postURL = "http://localhost:3000/employees";
